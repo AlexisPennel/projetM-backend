ae7731b6657c9c0623caf781f9da7890c556fe87
Alexis Pennel <alexispennel@gmail.com>
2023-03-13 20:51:40 -0600

    correction

--- app.js
+++ app.js
@@ -23,12 +23,7 @@ mongoose.connect(`${process.env.MONGODB_URI}`,
 // Package helmet
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 
-//React
-app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
 // Prise en charge JSON
 app.use(express.json());
 
